73fa62b8d959601be013551a940231be3878b437
Jaydee Min <jaydeemin@gmail.com>
2013-06-28 03:07:22 -0700

    CH4.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,23 +11,28 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title("jaydeeminSampleApp | Home")
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("jaydeeminSampleApp")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
 
   end
 
   describe "Help page" do
 
-  	it "shoould have the content 'Help'" do
+  	it "should have the content 'Help'" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
 
   	it "should have the title 'Help'" do
     	visit '/static_pages/help'
-    	expect(page).to have_title("jaydeeminSampleApp | Help")
+    	expect(page).to have_title("Help")
   	end
 
   end
@@ -42,9 +47,25 @@ describe "StaticPages" do
 
   	it "should have the title 'About Us'" do
     	visit '/static_pages/about'
-    	expect(page).to have_title("jaydeeminSampleApp | About Us")
+    	expect(page).to have_title("About Us")
     end
 
   end
+
+  describe "Contact Page" do
+
+  	it "should have the content 'Contact'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_content('Contact')
+  	end
+
+  	it "should have the title 'Contact'" do
+    	visit '/static_pages/contact'
+    	expect(page).to have_title("Contact")
+    end
+
+  end
+
+
 
 end
